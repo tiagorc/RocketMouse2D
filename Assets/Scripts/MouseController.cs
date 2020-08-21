16e31db9cc77f60c6b6517c11cdc1a44ad84f17c
tiagorc <eulercarvalho.se@gmail.com>
2020-08-20 21:00:28 -0300

    Initial commit with player, control script, celling and floor

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    //Variables
+    public float jetpackForce = 75.0f;
+    public Rigidbody2D playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+        if (jetpackActive)
+        {
+            playerRigidbody.AddForce(new Vector2(0, jetpackForce));
+        }
+    }
+}
